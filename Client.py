6f17ec799f43d5e80e057edd22d9005c97bf8e94
ragabala <ragavendranb92@gmail.com>
2018-04-15 02:23:52 -0400

    adding p2p app

---
+++ Client.py
@@ -0,0 +1,270 @@
+import socket
+import platform
+import os
+import sys
+import email.utils
+import time
+from pathlib import Path
+from threading import Thread
+
+
+P2P_VERSION = "P2P-CI/1.0"
+RFC_FILES_FOLDER = os.getcwd() + "/RFCFiles/rfc"
+SERVER_PORT_NUM = 7734 # the server_port and the address are according to the requirements
+SERVER_HOST_NAME = 'localhost' # change the below to other IP if the server is running in any other system. Here we will be running both the client and the server in the same system.
+
+
+client_port_number = int(sys.argv[1])
+#client_port_number = 60000
+
+def upload_rfc_from_peer():
+    '''This is the upstream that handles any peer request for an RFC. This is constantly running in a seperate thread from main thread.
+        This is the response to the get request
+        P2P-CI/1.0 200 OK
+        Date: Wed, 12 Feb 2009 15:12:05 GMT
+        OS: Mac OS 10.2.1
+        Last-Modified: Thu, 21 Jan 2001 9:23:46 GMT
+        Content-Length: 12345
+        Content-Type: text/text
+        (data data data ...)
+    '''
+
+    upload_server_socket = socket.socket()
+    upload_server_socket.bind(('localhost',client_port_number))
+    upload_server_socket.listen(10)
+    while True:
+        incoming_socket,incoming_addr = upload_server_socket.accept()
+        data = str(incoming_socket.recv(1024).decode()).strip()
+        print("rcvd data ",data)
+        request = data.split("\r\n")
+        if len(request) == 3 and request[0].startswith('GET RFC') and request[1].startswith('Host:') and request[2].startswith('OS:'):
+            if P2P_VERSION not in request[0]:
+                incoming_socket.sendall(str("505 P2P-CI Version Not Supported\r\n").encode())
+            else:
+                rfc_requested = request[0].split(" ")[2]
+                rfc_file_location = RFC_FILES_FOLDER+rfc_requested+".txt"
+                payload = open(rfc_file_location,'r').read()
+                response = str(P2P_VERSION + " 200 OK\r\n" +\
+                            "Date: " + str(email.utils.formatdate(usegmt=True)) +"\r\n" +\
+                            "OS: " + platform.platform() +"\r\n" +\
+                            "Last-Modified: " + str(time.ctime(os.path.getmtime(rfc_file_location))) +"\r\n" +\
+                            "Content-Length: " + str(len(payload)) +"\r\n" +\
+                            "Content-Type: text/text\r\n" + \
+                            payload)
+                print("responding with ",response)
+                incoming_socket.sendall(response.encode())
+        else:
+            incoming_socket.sendall(str("400 Bad Request\r\n").encode())
+
+
+def send_get_request_to_peer(peer_host_name, peer_port_num, rfc_num, rfc_title, get_request):
+    '''This method is used for making requests to any peer after getting the information from the server regarding the peer'''
+
+    download_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    download_socket.connect((peer_host_name,int(peer_port_num)))
+    download_socket.sendall(str(get_request).encode())
+    data = download_socket.recv(1024).decode()
+    response = data.split("\r\n")
+
+    if P2P_VERSION+ ' 200 OK' in response[0]:
+        content_length = response[4]
+        content_length = int(content_length[16:])
+        data = download_socket.recv(content_length).decode()
+        data = data[data.find('text/text\r\n')+12:]
+        rfc_file_location = os.getcwd() + "/Downloads/" + rfc_num + ".txt"
+        with open(rfc_file_location,'w') as file:
+            file.write(data)
+            print('File ',rfc_num+'.txt is downloaded' )
+    else:
+        print(response[0])
+    download_socket.close()
+
+def user_choices(choice,P2Ssocket):
+    '''This method is a hook method for the make requests . This is used for getting additional information that are required for the requests'''
+    print('--------------------------------------------------------------------------------')
+    print("Type Back to get the option menu and Exit to close")
+
+    try:
+        choice = int(choice)
+    except ValueError:
+        print("Please Enter a valid number")
+        return True
+
+    # Choices 1,2,3 are made from the Client to the server
+    if choice == 1:
+        print("This choice is used for registering the RFC of the client in the server")
+        user_input = input("enter the input in the following format ADD <RFC_NUM> <RFC_TITLE> " )
+    elif choice == 2:
+        print("This choice is used for getting all the peers that serves the Given RFC  ")
+        user_input = input("enter the input in the following format LOOKUP <RFC_NUM> <RFC_TITLE> ")
+    elif choice == 3:
+        user_input = "LOOK ALL"
+    # Choice 4
+    elif choice == 4:
+        print("This choice is used for getting an RFC from a Peer after connecting ")
+        user_input = input("enter the input in the following format GET <RFC_NUM> <RFC_TITLE>  ")
+    # Default case
+    else:
+        user_input = 'Exit'
+
+    if user_input == 'Back':
+        return True
+
+    return make_requests(P2Ssocket, user_input, choice)
+
+
+def make_requests(P2Ssocket, input, choice):
+    '''This method executes the request according to the user input and returns a boolean(True) for continuation and false for exiting'''
+    if choice > 4:
+        if(input == 'Exit'):
+            P2Ssocket.sendall(str(input).encode())
+            response = P2Ssocket.recv(1024).decode()
+            print(" --- Server Responded with ---------------------------------------------------")
+            print(response)
+            print(" -----------------------------------------------------------------------------")
+        return False
+
+    # for adding a New RFC to server
+    if choice == 1:
+        input_values = input.split(' ')
+        if len(input_values) != 3 and not input.startswith('ADD'):
+            print("Please enter the format as ADD <RFC_NUM> <RFC_TITLE> next time")
+            return True
+        rfc_num = str(input_values[1])
+        rfc_title = str(input_values[2])
+        rfc_file_location = Path(RFC_FILES_FOLDER+rfc_num+".txt")
+        try:
+            rfc_file_location.resolve()
+        except FileNotFoundError:
+            print("given rfc dosen't exist to be added to the server")
+            return True
+        else:
+            # file exists and we begin the add process
+            request = add_rfc_method(rfc_num,rfc_title)
+            print("Sending print request to Server")
+            P2Ssocket.sendall(str(request).encode())
+            response = P2Ssocket.recv(1024).decode()
+            print(" --- Server Responded with ---------------------------------------------------")
+            print(response)
+            print(" -----------------------------------------------------------------------------")
+            return True
+
+
+    #for listing all RFCs from server
+    elif choice == 3:
+        request = list_all_method()
+        P2Ssocket.sendall(str(request).encode())
+        response = P2Ssocket.recv(1024).decode()
+        print(" --- Server Responded with ---------------------------------------------------")
+        print(response)
+        print(" -----------------------------------------------------------------------------")
+        return True
+
+    #for looking up an RFC from server
+    elif choice == 2 or choice == 4:
+        input_values = input.split(' ')
+        if len(input_values) != 3:
+            print("Please enter the format as LOOKUP <RFC_NUM> <RFC_TITLE> next time")
+            return True
+        rfc_num = input_values[1]
+        rfc_title = input_values[2]
+
+        request = lookup_rfc_method(rfc_num, rfc_title)
+        P2Ssocket.sendall(str(request).encode())
+        response = P2Ssocket.recv(1024).decode()
+        print(" --- Server Responded with ---------------------------------------------------")
+        print(response)
+        print(" -----------------------------------------------------------------------------")
+        if choice == 2:
+            return True
+        else:
+            # This is for getting the file from the peer (choice 4)
+            response_lines = response.split("\r\n")
+            if '404' in response_lines[0] or "Not Supported" in response_lines[0] or "Bad" in response_lines[0]:
+                print("Unable to perform GET request")
+                return True
+            response_peer_information  = response_lines[2].split(" ") # this is the start of the peer information and can span multiple lines depending on the number of peers having that RFC
+
+            peer_host_name = response_peer_information[3]
+            peer_port_num = response_peer_information[4]
+            get_request = get_rfc_method(rfc_num)
+            Thread(target= send_get_request_to_peer,args=(peer_host_name,peer_port_num,rfc_num,rfc_title,get_request)).start()
+            return True
+    else:
+        return False
+
+
+
+# below are the functions that represent the functions for getting the request format for a particular type of method
+# between Peers P2P
+def get_rfc_method(rfc_number):
+    ''' This Method is used for returning the format of the RFC request from one peer. Example of the request is as follows
+        GET RFC 1234 P2P-CI/1.0
+        Host: somehost.csc.ncsu.edu
+        OS: Mac OS 10.4.1 '''
+    return "GET RFC " + str(rfc_number) +" " + str(P2P_VERSION) + "\r\n" + \
+            "Host: " + str(socket.gethostname()) +"\r\n" +\
+            "OS: " + str(platform.platform()) +"\r\n"
+
+
+# Between a client and a server P2S
+def add_rfc_method(rfc_number,rfc_title):
+    '''This Method is used for adding an rfc to the index of the server. The request format is as follows
+        ADD RFC 123 P2P-CI/1.0
+        Host: thishost.csc.ncsu.edu
+        Port: 5678
+        Title: A Proferred Official ICP
+        ADD RFC 2345 P2P-CI/1.0 '''
+    return "ADD RFC " + str(rfc_number) +" " + P2P_VERSION + "\r\n" + \
+            "Host: " + str(socket.gethostname()) +"\r\n" + \
+            "Port: " + str(client_port_number) +"\r\n" +\
+            "Title: " + str(rfc_title) +"\r\n"
+
+def lookup_rfc_method(rfc_number,rfc_title):
+    ''' This method is used to get the index of all the peers for a given RFC from the server
+        LOOKUP RFC 3457 P2P-CI/1.0
+        Host: thishost.csc.ncsu.edu
+        Port: 5678
+        Title: Requirements for IPsec Remote Access Scenarios '''
+    return "LOOKUP RFC " + str(rfc_number) +" " + str(P2P_VERSION) + "\r\n" + \
+            "Host: " + str(socket.gethostname()) +"\r\n" + \
+            "Port: " + str(client_port_number) +"\r\n" + \
+            "Title: " + str(rfc_title) +"\r\n"
+
+def list_all_method():
+    '''This method is used to get all the peers information from the server
+        LIST ALL P2P-CI/1.0
+        Host: thishost.csc.ncsu.edu
+        Port: 5678 '''
+    return "LIST ALL " +str(P2P_VERSION) + "\r\n" + \
+            "Host: " + str(socket.gethostname()) +"\r\n" + \
+            "Port: " + str(client_port_number) +"\r\n"
+
+
+
+def user_interface(P2Ssocket):
+    '''This is the main function that is used by the client for making any file requests. This is where the user inputs his option as client and peer/server responds'''
+    print('--------------------------------------------------------------------')
+    option_list = "Please select one of the various options \r\n"+\
+                    "The below options 1,2,3 is for requesting the SERVER for information regarding the peers/rfcs\r\n"+\
+                    "1.ADD\r\n"+\
+                    "2.LOOKUP\r\n"+\
+                    "3.LOOKALL\r\n"+\
+                    "The below option is used for requesting the PEER\r\n"+\
+                    "4.GET\r\n"+\
+                    ">= 5 .Exit  \r\n"
+
+    while True:
+        choice = input(option_list)
+        if not user_choices(choice,P2Ssocket):
+            break
+
+
+
+if __name__ == '__main__':
+    '''This is the starter main method that connects to the server and passes the server socket to the required methods'''
+    P2Ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    P2Ssocket.connect(('', SERVER_PORT_NUM))
+    P2Ssocket.sendall(str(client_port_number).encode()) # letting the server know the portnumber the client is going to use
+    Thread(target=upload_rfc_from_peer).start()
+    user_interface(P2Ssocket)
---
+++ server.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+import socket
+from threading import Thread
+
+# {statuscode: phrase}
+status = {'200': 'OK', '400': 'Bad Request', '404': 'Not Found', '505': 'P2P-CI Version Not Supported'}
+# {hostname: port}
+active_peers = {}
+# {rfcnumber: rfctitle}
+rfc_info = {}
+# {rfcnumber: [hostname1, hostname2..]}
+rfc_peer_map = {}
+
+version = "P2P-CI/1.0"
+server_port = 7734 # predefined well known port
+server_name = socket.gethostname()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('', server_port))
+server_socket.listen(1)
+
+def peers_with_a_rfc(rfcnumber):
+    '''Returns a list of peers having a particular RFC'''
+    global rfc_info
+    if rfcnumber in rfc_info: # if this RFC exists
+        message = version + " 200 " + status.get('200') + "\r\n\r\n" # for successful request
+        peerlist = rfc_peer_map.get(rfcnumber) # get list of peers having the RFC
+        tempmsg =''
+        rfcdata = "RFC " + rfcnumber + " " + rfc_info.get(rfcnumber) + " "
+        for peer in peerlist: # generate the message for each peer
+            tempmsg += rfcdata + peer + "\r\n"
+        message += tempmsg
+    else: # if RFC is not found
+        message = version + " 404 " + status.get('404') + "\r\n"
+    return message
+
+
+def add_peer_to_rfc(rfcnumber, rfctitle, hostname):
+    '''Adding a new RFC if not exists and adding peer to a particular RFC'''
+    global rfc_peer_map,rfc_info
+    if rfcnumber not in rfc_info: # if RFC is not present in list
+	    rfc_info[rfcnumber]=rfctitle # store the RFC title
+	    rfc_peer_map[rfcnumber]=[hostname] # generate the new entry with RFC number as key with hostname as value
+    else:
+        peer_list=rfc_peer_map.get(rfcnumber) # get list of peers having the RFC
+        peer_list.append(hostname) # add new peer to the peer_list
+    print("Added RFC successfully")
+    message = version + " 200 " + status.get('200') + "\r\n" + "RFC " + rfcnumber + " " + rfctitle + " " + hostname + " " + active_peers.get(hostname) + "\r\n" # for successful request
+    return message
+
+def register_new_peer(hostname, port):
+    '''Add a new peer to list of active peers'''
+    global active_peers
+    active_peers[hostname] = port # register new hostname with its port
+
+
+
+def display_rfc_and_peers(hostname):
+    '''Displays the list of all peers and RFC details'''
+    global rfc_peer_map, rfc_info
+    rfclist = rfc_peer_map.keys() # get list of RFCs
+    if rfclist:
+        message = version + " 200 " + status.get('200') + "\r\n\r\n" # success message
+       # print("RFC LIST : ",rfclist)
+
+        for rfc in rfclist: # for each RFC
+            peerlist=rfc_peer_map.get(rfc) # get peers having this RFC
+            rfcmsg = "RFC: " + str(rfc) + " " + str(rfc_info.get(rfc))
+            peermsg=''
+            for peer in peerlist:
+                peermsg += rfcmsg + " " + peer + "\r\n"
+            message += peermsg
+    else: # if no RFC found
+        message = version + " 404 " + status.get('404') + "\r\n"
+    return message
+
+def p2p_server(conn, addr):
+    '''Validates the client request, performs necessary action and returns the response'''
+    request = conn.recv(1024).decode()
+    print("received request ",request.strip())
+    portnumber = request.strip()
+    hostname = addr[0]+" "+portnumber
+    register_new_peer(hostname, portnumber)
+
+    while True:
+        newrequest = conn.recv(1024).decode()
+        print("received request ",newrequest)
+        hostversion = newrequest.split('\r\n')[0].split(' ')[-1:]
+        hostversion = hostversion[0]
+        if version != hostversion: # if version mismatch
+            message = "505 " + status.get('505') + "\r\n"
+
+        elif "Host: " in newrequest.split('\r\n')[1] and "Port: " in newrequest.split('\r\n')[2]: # check for correct request format
+            if newrequest.split('\r\n')[0].split(' ')[0] == "EXIT":
+                break
+
+            if newrequest.split('\r\n')[0].split(' ')[0] == "ADD" and newrequest.split('\r\n')[0].split(' ')[1] == "RFC" and "Title: " in newrequest.split('\r\n')[3]:
+                rfcnumber = newrequest.split('\r\n')[0].split(' ')[2]
+                rfctitle = newrequest.split('\r\n')[3].split(': ')[1]
+                message = add_peer_to_rfc(rfcnumber, rfctitle, hostname)
+
+            elif newrequest.split('\r\n')[0].split(' ')[0] == "LOOKUP" and newrequest.split('\r\n')[0].split(' ')[1] == "RFC" and "Title: " in newrequest.split('\r\n')[3]:
+                rfcnumber = newrequest.split('\r\n')[0].split(' ')[2]
+                rfctitle = newrequest.split('\r\n')[3].split(': ')[1]
+                message = peers_with_a_rfc(rfcnumber)
+
+            elif newrequest.split('\r\n')[0].split(version)[0] == "LIST ALL ":
+                message = display_rfc_and_peers(hostname)
+            else:
+                message = " 400 " + status.get('400') + "\r\n" # bad request format
+        else:
+            message = " 400 " + status.get('400') + "\r\n" # bad request format
+
+        conn.send(str(message).encode()) # tranfer the response to client
+
+
+    # Remove the client when it closes connection
+    for rfc in rfc_peer_map.keys(): # for each RFC in available RFC list
+        peerlist=rfc_peer_map.get(rfc) # peer list of each RFC
+        if hostname in peerlist: # if hostname is present
+            if len(peerlist) > 1: # if other hosts are there apart from the client which closes connection
+                peerlist.pop(hostname) # remove the client from list
+                #global rfc_peer_map
+                rfc_peer_map[rfc]=peerlist # uodate the dictionary for the RFC
+            else: # if this is only client hving this RFC
+                #global rfc_peer_map, rfc_info
+                rfc_peer_map.pop(rfc, None) # remove the RFC entry from RFC client dictionary
+                rfc_info.pop(rfc) # remove RFC entry from list of known RFCs
+
+
+
+    if hostname in active_peers.keys(): # to remove the client from active peer list
+        #global active_peers
+        active_peers.pop(hostname, None)
+
+    conn.close() # close the connection after the client's information is deleted
+
+
+if __name__ == '__main__':
+    '''This makes the server to run and handle each request from client asynchronously'''
+    print("Server Listening at Port :",server_port)
+    while True: # keeps running to handle new clients
+        conn, addr = server_socket.accept() # accept the client connection
+        print("New connection: ", addr)
+        Thread(target=p2p_server, args=(conn, addr)).start() # spawn a new thread for each client
+
+    server_socket.close()
